42cca674706747e1177d10faeca01f5aec1f7718
Alexander3006 <ukrpresident2036@gmail.com>
2021-02-10 20:57:16 +0200

    lab5

---
+++ src/pages/Images.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  View,
+  Alert,
+} from 'react-native';
+import {Header} from 'react-native-elements';
+import {useState} from 'react';
+import {ImagesGrid} from '../components/ImagesGrid';
+import * as ImagePicker from 'react-native-image-picker';
+
+const chooseImage = (addImage: (a: string) => void) => {
+  const options = {
+    noData: true,
+  };
+  ImagePicker.launchImageLibrary(options, (response) => {
+    if (!response.uri) return;
+    addImage(response.uri);
+  });
+};
+
+export const Images = () => {
+  const [images, setImages] = useState<string[][]>([[]]);
+
+  const addImage = (image: string) => {
+    const lastImageSet = images.pop() ?? [];
+    if (lastImageSet.length < 7) {
+      const data = [...lastImageSet, image];
+      setImages([...images, data]);
+      return;
+    }
+    setImages([...images, lastImageSet, [image]]);
+  };
+
+  const renderItem = ({item, index}: any) => {
+    return <ImagesGrid images={item} key={index} />;
+  };
+  return (
+    <>
+      <Header
+        backgroundColor={'white'}
+        rightComponent={{icon: 'add', onPress: () => chooseImage(addImage)}}
+      />
+      <FlatList
+        data={images}
+        style={styles.container}
+        renderItem={renderItem}
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: 'red',
+  },
+});
